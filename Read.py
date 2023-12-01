dce401e33df65d27f66f05f691b704e6179815c3
samshurp <151703264+samshurp@users.noreply.github.com>
2023-11-30 22:53:03 -0800

    Update Read.py
    Latest data with pixel manipulation give the position.

--- Read.py
+++ Read.py
@@ -48,6 +48,39 @@ with open('/Users/sam_sherpa/Desktop/Python txt/digitdata/data2', 'rb') as fid:
         Pprob = V1 / TotalV1
         print(Pprob) 
 
+********************************* 
+******** Latest - Nov 30 ******
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+# For number 0 
+data_mean = []
+data_std = []
+
+matrix = 28
+threshold = 128
+base_path = '/Users/sam_sherpa/Desktop/Python txt/digitdata/'
+Sum_vector = np.zeros((matrix * matrix,))
+V = {}
+
+with open('/Users/sam_sherpa/Desktop/Python txt/digitdata/data0', 'rb') as fid: 
+    for i in range(1000):
+        img = np.fromfile(fid, dtype=np.uint8, count=matrix*matrix).reshape((matrix, matrix))
+        # plt.imshow(img)
+        # plt.show()
+        binary_image = np.where(img >= threshold, 1, 0)
+        V[i] = binary_image.flatten()
+
+    matrix = np.vstack([V[i] for i in range(1000)]) 
+    # Calculate mean and standard deviation for each column
+    meanColumn = np.mean(matrix, axis=0)
+    stdColumn = np.std(matrix, axis=0)
+    Print('')
+    print(meanColumn)
+    print('Next- Standard Deviation')
+    print(stdColumn)
+
 
 
 
